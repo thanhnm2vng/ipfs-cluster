a21b2a269ac05a576e74dd8ad85216baac74c9a5
thanhnm2@vng.com.vn <thanhnm2@vng.com.vn>
2022-06-29 06:00:30 +0000

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -43,8 +43,8 @@ else
     ipfs-cluster-service init --consensus "${IPFS_CLUSTER_CONSENSUS}"
 fi
 
-# exec ipfs-cluster-service --debug $@
-while true
-do
- sleep 100
-done
+exec ipfs-cluster-service --debug $@
+# while true
+# do
+#  sleep 100
+# done
